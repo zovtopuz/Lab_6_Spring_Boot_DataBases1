4b1f30d743fa5a2437b42747a0ec5a0639fd9cfc
Роман <zovtopuz@MacBook-Pro-Roman.local>
2020-06-02 20:29:40 +0300

    first commit

--- src/main/java/ua/lviv/iot/config/SwaggerConfiguration.java
+++ src/main/java/ua/lviv/iot/config/SwaggerConfiguration.java
@@ -14,7 +14,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class SwaggerConfiguration {
     private static final String SWAGGER_API_VERSION = "1.0";
     private static final String LICENSE_TEXT = "License";
-    private static final String title = "Spring Boot Start by Serhii Senyk";
+    private static final String title = "Spring Boot Start by Roman Seneshyn";
     private static final String description = "Documentation for the project";
 
     private ApiInfo apiInfo() {
